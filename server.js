97c047db7b8da6ca9e31b49f47a68fa914ef031c
neiljparikh <147933251+neiljparikh@users.noreply.github.com>
2024-01-03 15:47:50 -0800

    Added additional routes for errors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //DEPENDENCIES
 const express = require("express");
-
+const path = require("path");
 
 //DATA
 
@@ -10,9 +10,32 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //MIDDLEWARE
-
+// stuff to get the req.body
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//static assets
+app.use(express.static("public"));
 
 //ROUTES
+// HTML routes - deliver pages
+// GET / - the home page
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+  );
 
+// API Routes - deliver data
+app.get("/api/todos", (req, res) => 
+    res.json([
+        {
+            title: "Buy Milk",
+        }
+    ])
+);
+
+//404 route
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/404.html"));
+})
+ 
 //START SERVER
 app.listen(PORT, () => console.log(`Server live on port ${PORT}`))
